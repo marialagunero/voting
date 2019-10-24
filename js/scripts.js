57b072c5dcd95c252713bcde086afdc56fe8e5c0
Maria Lagunero <maria58@gmail.com>
2019-10-23 19:30:28 -0700

    added text to html. css, and js

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,10 @@
 $(document).ready(function() {
-  $("#formA").submit(function(event) {
-    var name = $("input#name").val();
-    $(".name").text(name);
+  var age = parseInt(prompt("How old are you?"));
 
-    var description = $("input#description").val();
-    $(".description").text(description);
-
-    var date = $("input#date").val();
-    $(".date").text(date);
-
-    var starttime = $("input#starttime").val();
-    $(".starttime").text(starttime);
-
-
-
-
-
-
-
-
-
-
-
-    $(".Success").show();
-
-    event.preventDefault();
-  });
+  if (age >=18) {
+    alert("Let's Vote");
+    $('#adults').show();
+  } else {
+    $('#minors').show();
+  }
 });
